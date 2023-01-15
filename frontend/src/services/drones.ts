65179e601a2aa7f93226036417f71abb0df86592
Sherkhan Azimov <sherkhanazim@gmail.com>
2023-01-16 00:12:12 +0200

    fix(): change ep

--- frontend/src/services/drones.ts
+++ frontend/src/services/drones.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_ROOT = 'http://localhost:5000/';
+const API_ROOT =
+  'https://startlingly-rubber-baboon-penthouse-dev.wayscript.cloud/';
 
 export const getDrones = () => {
   return axios.get(`${API_ROOT}`).then((res) => res.data);
